f48644ff5e5a4a8782616a273c546349f3114c5e
boram <boram910530@gmail.com>
2024-02-19 19:22:59 +0900

    react

---
+++ DAY007-8/state-event-app/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg'
+import './App.css'
+
+import ReferTypeEvent from './ReferTypeEvent'
+import ProductManager from './ProductManager'
+
+function App() {
+    return (
+        <div className="App">
+            <ReferTypeEvent></ReferTypeEvent>
+            <hr></hr>
+            <ProductManager></ProductManager>
+        </div>
+    )
+}
+
+export default App
