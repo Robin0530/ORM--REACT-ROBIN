d5a3126860e65e94f3d5302e2cc312d44444d207
boram <boram910530@gmail.com>
2024-02-20 10:06:26 +0900

    edit

--- DAY005/jsx-react-app/src/App.js
+++ DAY005/jsx-react-app/src/App.js
@@ -3,66 +3,69 @@ import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
 
-// 자식 컴포넌트 참조하기
+//자식 컴포넌트 참조하기
 
-// 함수형 자식 컴포넌트를 참조합니다.
+//함수형 자식 컴포넌트를 참조합니다.
 import FChild from './FunctionChild'
 
-// 클래스형 자식 컴포넌트를 참조합니다.
+//클래스형 자식 컴포넌트를 참조합니다.
 import CChild from './ClassChild'
 
-// 내프로필 컴포넌트 참조하기
+//내프로필 컴포넌트 참조하기
 import MyProfile from './Profile'
 
-// 함수형 컴포넌트 생성하기
-// 리액트 UI 컴포넌트는 반환값으로 JSX UI 요소정보를 반환하여 최종 웹브라우저에 UI를 표현합니다.
-// JSX UI요소를 제어하기 위해 함수내에 기능구현 영역에서 필요한 데이터와 이벤트를 처리합니다.
+import CounterFunction from './CounterFunctionComponent'
+
+import CounterClass from './CouterClassComponent'
+
+//함수형 컴포넌트 생성하기
+//리액트 UI컴포넌트는 반환값으로 JSX UI요소정보를 반환하여 최종 웹브라우저에 UI를 표현합니다.
+//JSX UI요소를 제어하기 위해 함수내에 기능구현 영역에서 필요한 데이터와 이벤트를 처리합니다.
 function App() {
-    // 기능구현 코드
-    // 이벤트 기능 구현
-    // 해당 컴포넌트의 STATE(로컬데이터) 값과 JSX영역내 UI요소간 데이터 바인딩을 통해
-    // 데이터 바인딩 기법으로 UI요소와 데이터를 편리하게 관리합니다.
+    //기능구현코드
+    //이벤트 기능 구현
+    //해당 컴포넌트의 STATE(로컬데이터) 값과 JSX영역내 UI요소간 데이터 바인딩을 통해
+    //데이터 바인딩 기법(MVVM패턴기반)으로 UI요소와 데이터를 편리하게 관리합니다.
 
-    // 샘플용 로컬 문자열 테스트 데이터(스테이트) 정의 및 초기값 할당하기
-    const [Sample, setSample] = useState('샘플데이터')
+    //샘플용 로컬 문자열 테스트 데이터(스테이트) 정의 및 초기값 할당하기
+    const [sample, setSample] = useState('샘플데이터')
 
-    // 사용자 정보 JSON 데이터 상태값 정의 및 할당
-    const [user, serUser] = useState({
-        userid: 'robinsss',
-        name: '보람',
-        email: 'rrr@gmail.com',
-        telephone: '010-2256-5222',
-        address: '경기도 군포시',
+    //사용자 정보 JSON 데이터 상태값 정의 및 할당
+    const [user, setUser] = useState({
+        userid: 'gabriel',
+        name: '가브리엘',
+        email: 'eddy@msoftware.co.kr',
+        telephone: '011-555-6666',
+        address: '경기도 성남시시',
     })
 
     return (
         <div>
-            <h1>안녕하세요. {Sample} </h1>
-            {/* 각종 UI컴포넌트의 반환결과값이 결국 JSX 요소들이기 때문에 참조한 UI는 결국 부모컴포넌트에 JSX영역에서
-                사용된다. */}
-            {/* 함수형 자식 컴포넌트 결과물 (JSX요소) 표시하기 */}
-            <FChild companyName="엠소프트웨어" telephone="01011112222" address="인천시 부평구" SampleData={Sample}>
+            <h1>안녕하세요. 엠소프트웨어 방문을 환영합니다.{sample}</h1>
+            {/* 각종 UI컴포넌트의 반환결과값이 결국 JSX요소들이기 때문에 참조한 UI는 결국 부모컴포넌트에 JSX영역에서 사용된다. */}
+            {/* 함수형 자식 컴포넌트 결과물(JSX요소)표시하기 */}
+            <FChild
+                companyName="엠소프트웨어"
+                telephone="02-555-6666"
+                address="서울시 테헤란로 스팍선릉점"
+                sampleData={sample}
+            >
                 우리회사소개
             </FChild>
-            <br />
-            <hr />
-            {/* 클래스형 자식 컴포넌트 결과물 (JSX요소) 표시하기 */}
-            <CChild deptName="개발1팀" deptRole="협업툴솔루션개발" Employeee={10} SampleData={Sample}>
+            -{/* 클래스형 자식 컴포넌트 결과물(JSX요소)표시하기 */}
+            <CChild deptName="개발1팀" deptRole="협업툴솔루션개발" employeee={10} sampleData={sample}>
                 개발팀소개
             </CChild>
-            <br />
-            <hr />
             {/* 내 프로필 자식 컴포넌트 표시하기 */}
-            <MyProfile userid="robin" name="이보람" email="boram910530@gmail.com" telephone="01022565222" age={30}>
+            <MyProfile userid="eddy" name="강창훈" email="eddy@test.co.kr" telephone="010-2760-5246" age="서른">
                 내 소개 페이지
             </MyProfile>
-
-            <br />
-            <hr />
             {/* 내 프로필 자식 컴포넌트 표시하기 */}
-            <MyProfile userid={user.userid} name={user.name} email={user.email} telephone={user.telephone} age={30}>
-                스테이트기반 내 소개 페이지
+            <MyProfile userid={user.userid} name={user.name} email={user.email} telephone={user.telephone} age="마흔">
+                스테이기반 내 소개 페이지
             </MyProfile>
+            <CounterFunction></CounterFunction>
+            <CounterClass></CounterClass>
         </div>
     )
 }
--- DAY007-8/state-event-app/src/ProductManager.js
+++ DAY007-8/state-event-app/src/ProductManager.js
@@ -1,7 +1,7 @@
 import React, { useState } from 'react'
 
 const ProductManager = () => {
-    // 단일 상품정보 데이터 구조 정의 및 초기화
+    //단일 상품정보 데이터 구조 정의 및 초기화
     const [product, setProduct] = useState({
         productName: '',
         price: 0,
@@ -9,25 +9,34 @@ const ProductManager = () => {
         manufacturer: '',
     })
 
-    // 상품목록 데이터 구조 정의 및 초기화
+    //상품목록 데이터 구조 정의 및 초기화
     const [productList, setProductList] = useState([])
 
-    // 제품정보 입력처리 이벤트 핸들러
+    //제품정보 입력처리 이벤트 핸들러
     const handleProduct = (e) => {
         setProduct({ ...product, [e.target.name]: e.target.value })
     }
 
-    // 저장버튼 클릭 이벤트 핸들러 함수
-    const handleSave = (e) => {
-        // 기존 상품 배열 목록에 등록하려는 단일상품객체의 복사본을 만들어 추가해준다.
-        // 기존배열의 복사본(productList)에 새로운 객체를 추가(product)
+    //저장버튼 클릭 이벤트 핸들러 함수
+    const handleSave = () => {
+        //기본 상품 배열 목록에 등록하려는 단일상품객체의 복사본을 만들어 추가해준다.
         setProductList([...productList, { ...product }])
-        handleInit(e)
+        handleInit()
     }
 
-    // 상품목록 생성 함수 정의
+    //초기화 버튼 클릭 이벤트 핸들러
+    const handleInit = () => {
+        setProduct({
+            productName: '',
+            price: 0,
+            stock: 0,
+            manufacturer: '',
+        })
+    }
+
+    //상품목록 생성 함수 정의
     const productUIList = productList.map((pro, i) => (
-        // UI요소에 반복적으로 map을 이용해 바인딩하는 경우 UI요소에 key속성을 유일값으로 바인딩해줘야한다.
+        // ui요소에 반복적으로 map을 이용해 바인딩하는경우 ui요소에 key속성을 유일값으로 바인딩해줘야한다.
         <tr key={i}>
             <td>{pro.productName}</td>
             <td>{pro.price}</td>
@@ -40,39 +49,35 @@ const ProductManager = () => {
         </tr>
     ))
 
-    // 단일 상품정보 선택시 상단 폼에 선택정보 표시하기
+    //단일 상품정보 선택시 상단 폼에 선택정보 표시하기
     const handleSelect = (pro) => {
-        // 배열에서 선택한 단일 상품정보로 폼에 바인딩된 단일상품정보로 변경처리한다.
+        //배열에서 선택한 단일 상품정보로 폼에 바인딩된 단일상품정보로 변경처리한다.
         setProduct(pro)
     }
 
-    // 상품 목록 배열에서 선택 상품정보 삭제처리하고 재바인딩하기
+    //상품 목록 배열에서 선택 상품정보 삭제처리 하고 재 바인딩하기
     const handleRemove = (pro) => {
-        // 배열데이터에서 특정조건에 해당하는 데이터 복사본 목록을 조회할때는 filter()메소드 사용
-        // 배열안에 제품명이 삭제하려는 제품명과 다른것으로 조회하면 결국 삭제하려는 아이템만 빼고 모두 조회하는 결과물이 만들어집니다.
+        //배열데이터에서 특정조건에 해당하는 데이터 복사본 목록을 조회할떄는 filter()메소드 사용
+        //배열안에 제품명이 삭제하려는 제품명과 다른것으로 조회하면 결국 삭제하려는 아이템 만 뺴고 모두 조회하는 결과물이 만들어집니다.
         const filteredProductList = productList.filter((p) => p.productName !== pro.productName)
         setProductList(filteredProductList)
-    }
-
-    // 초기화 버튼 클릭 이벤트 핸들러 함수
-    const handleInit = (e) => {
-        setProduct({
-            productName: '',
-            price: 0,
-            stock: 0,
-            manufacturer: '',
-        })
     }
 
     return (
         <div>
             <h1>제품정보 관리화면</h1>
-            제품명: <input type="text" name="productName" value={product.productName} onChange={handleProduct}></input>
+            제품명:
+            <input type="text" name="productName" value={product.productName} onChange={handleProduct} />
             <br />
-            가격: <input type="text" name="price" value={product.price} onChange={handleProduct}></input> <br />
-            재고: <input type="text" name="stock" value={product.stock} onChange={handleProduct}></input> <br />
+            가격:
+            <input type="text" name="price" value={product.price} onChange={handleProduct} />
+            <br />
+            재고:
+            <input type="text" name="stock" value={product.stock} onChange={handleProduct} />
+            <br />
             제조사:
-            <input type="text" name="manufacturer" value={product.manufacturer} onChange={handleProduct}></input> <br />
+            <input type="text" name="manufacturer" value={product.manufacturer} onChange={handleProduct} />
+            <br />
             <hr></hr>
             <button onClick={handleSave}>저장</button>
             <button onClick={handleInit}>초기화</button>
@@ -82,15 +87,16 @@ const ProductManager = () => {
                     <tr>
                         <th>제품명</th>
                         <th>가격</th>
-                        <th>재고</th>
+                        <th>재고량</th>
                         <th>제조사</th>
                         <th>선택/삭제</th>
                     </tr>
                 </thead>
                 <tbody>
                     {/* {productUIList} */}
+
                     {productList.map((pro, i) => (
-                        // UI요소에 반복적으로 map을 이용해 바인딩하는 경우 UI요소에 key속성을 유일값으로 바인딩해줘야한다.
+                        // ui요소에 반복적으로 map을 이용해 바인딩하는경우 ui요소에 key속성을 유일값으로 바인딩해줘야한다.
                         <tr key={i}>
                             <td>{pro.productName}</td>
                             <td>{pro.price}</td>
