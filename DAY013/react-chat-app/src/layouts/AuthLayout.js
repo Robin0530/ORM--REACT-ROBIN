a8e08978d723b298bb95f08748df5660ca26f913
boram <boram910530@gmail.com>
2024-02-28 17:00:55 +0900

    react-chat-app

---
+++ DAY013/react-chat-app/src/layouts/AuthLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const AuthLayout = () => {
+    return (
+        <div>
+            <h1>인증된 페이지에서 호출되는 인증 레이아웃 영역</h1>
+        </div>
+    )
+}
+
+export default AuthLayout
---
+++ DAY013/react-chat-app/src/layouts/NonAuthLayout.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const NonAuthLayout = () => {
+    return (
+        <div>
+            <h1>비인증된 페이지에서 호출되는 레이아웃 영역</h1>
+        </div>
+    )
+}
+
+export default NonAuthLayout
